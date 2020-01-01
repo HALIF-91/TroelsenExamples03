b70e60329159a86c099316efa1439b7cbab0ebc5
ADIL <globalmail91@gmail.com>
2020-01-01 22:40:26 +0600

    создание освобождаемых объектов

---
+++ SimpleDispose/MyResourceWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimpleDispose
+{
+    internal class MyResourceWrapper : IDisposable
+    {
+        public void Dispose()
+        {
+            // Очистить неуправляемые ресурсы
+            // Освободить другие содержащиеся внутри освобождаемые объекты
+            // Только для целей тестирования
+            Console.WriteLine("******** In Dispose! *******");
+        }
+    }
+}
---
+++ SimpleDispose/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Fun with Dispose *****\n");
+
+            MyResourceWrapper rw = new MyResourceWrapper();
+            if(rw is IDisposable)
+                rw.Dispose();
+
+            DisposeFileSystem();
+
+            // метод Dispose() вызывается автоматически при выходе за пределы области действий
+            // допускается объявлять несколько объектов одного и того же типа
+            // при попытке применить using к объекту, не реализующему интерфейс IDisposable
+            // возникнет ошибка на этапе компиляции
+            using (MyResourceWrapper wrapper = new MyResourceWrapper(),
+                                    wrapper2 = new MyResourceWrapper())
+            {
+                // использовать объект wrapper и wrapper2
+            }
+
+            Console.ReadLine();
+        }
+        static void DisposeFileSystem()
+        {
+            FileStream fs = new FileStream("file.txt", FileMode.OpenOrCreate);
+
+            // вызовы этих методов делают одно и тоже
+            fs.Close();
+            fs.Dispose();
+        }
+    }
+}
