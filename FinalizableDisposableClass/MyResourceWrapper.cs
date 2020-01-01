440cda0f64c4e857424db1603aa1478b14b1faaa
ADIL <globalmail91@gmail.com>
2020-01-01 23:56:04 +0600

    создание финализируемых и освобождаемых типов

---
+++ FinalizableDisposableClass/MyResourceWrapper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FinalizableDisposableClass
+{
+    internal class MyResourceWrapper : IDisposable
+    {
+        // Используется для выяснения того, вызывался ли уже метод Dispose()
+        private bool disposed = false;
+        public string Str { get; set; } = "Hello";
+        // Пользователь объекта будет вызывать этот метод
+        // для очистки ресурсов как можно быстрее
+        public void Dispose()
+        {
+            // вызвать вспомогательный метод
+            // указание true означает, что очистку запустил пользователь объекта
+            CleanUp(true);
+
+            // если пользователь вызвал Dispose(), то финализация не нужна
+            // подавить финализацию
+            GC.SuppressFinalize(this);
+        }
+        private void CleanUp(bool disposing)
+        {
+            // удостовериться, не выполнялось ли уже освобождение
+            if (!this.disposed)
+            {
+                // если disposing равно true, освободить все управляемые ресурсы
+                if (disposing)
+                {
+                    // освободить управляемые ресурсы
+                    Str = null;
+                }
+                // очистить неуправляемые ресурсы
+            }
+            disposed = true;
+        }
+        // сборщик мусора будет вызывать этот метод, если
+        // пользователь объекта забыл вызвать Dispose()
+        ~MyResourceWrapper()
+        {
+            Console.Beep();
+            // вызвать вспомогательный метод
+            // указание false означает, что очистку запустил сборщик мусора
+            CleanUp(false);
+        }
+    }
+}
---
+++ FinalizableDisposableClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalizableDisposableClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Dispose() / Destructor Combo Platter *****");
+
+            // вызвать метод Dispose() вручную. Это не приведет к вызову финализатора
+            MyResourceWrapper rw = new MyResourceWrapper();
+            rw.Dispose();
+            
+            // не вызывать метод Dispose(). Это приведет к вызову финализатора
+            // и выдаче звукового сигнала
+            MyResourceWrapper rw2 = new MyResourceWrapper();
+            Console.ReadLine();
+        }
+    }
+}
