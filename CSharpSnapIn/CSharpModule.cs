999d531970f046d014ce0aab100591023578716c
ADIL <globalmail91@gmail.com>
2020-01-25 21:03:44 +0600

    расширяемое приложение

---
+++ CSharpSnapIn/CSharpModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CommonSnappableTypes;
+
+namespace CSharpSnapIn
+{
+    [CompanyInfo(CompanyName = "My Company", CompanyUrl = "www.metanit.com")]
+    public class CSharpModule : IAppFunctionality
+    {
+        void IAppFunctionality.DoIt()
+        {
+            MessageBox.Show("You have jsut used the C# snap-in!");
+        }
+    }
+}
---
+++ VbSnapIn/VbModule.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Windows.Forms
+Imports CommonSnappableTypes
+
+<CompanyInfo(CompanyName:="Chucky's Software", CompanyUrl:="www.chuckysoft.com")>
+Public Class VbModule
+    Implements IAppFunctionality
+
+    Public Sub DoIt() Implements IAppFunctionality.DoIt
+        MessageBox.Show("You have just used the VB snap in!")
+    End Sub
+End Class
