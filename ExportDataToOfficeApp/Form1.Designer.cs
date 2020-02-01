6a75a0f4e7ff03727d3933022b0fb22b3bcb0c34
ADIL <globalmail91@gmail.com>
2020-02-01 15:44:51 +0600

    взаимодействия с COM

---
+++ ExportDataToOfficeApp/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ExportDataToOfficeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddNewCar = new System.Windows.Forms.Button();
+            this.btnExportToExcel = new System.Windows.Forms.Button();
+            this.dataGridCars = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCars)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddNewCar
+            // 
+            this.btnAddNewCar.Location = new System.Drawing.Point(41, 389);
+            this.btnAddNewCar.Name = "btnAddNewCar";
+            this.btnAddNewCar.Size = new System.Drawing.Size(255, 29);
+            this.btnAddNewCar.TabIndex = 0;
+            this.btnAddNewCar.Text = "Add New Entry to Inventory";
+            this.btnAddNewCar.UseVisualStyleBackColor = true;
+            this.btnAddNewCar.Click += new System.EventHandler(this.btnAddNewCar_Click);
+            // 
+            // btnExportToExcel
+            // 
+            this.btnExportToExcel.Location = new System.Drawing.Point(472, 389);
+            this.btnExportToExcel.Name = "btnExportToExcel";
+            this.btnExportToExcel.Size = new System.Drawing.Size(279, 29);
+            this.btnExportToExcel.TabIndex = 1;
+            this.btnExportToExcel.Text = "Export Current Inventory to Excel";
+            this.btnExportToExcel.UseVisualStyleBackColor = true;
+            this.btnExportToExcel.Click += new System.EventHandler(this.btnExportToExcel_Click);
+            // 
+            // dataGridCars
+            // 
+            this.dataGridCars.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridCars.Location = new System.Drawing.Point(41, 68);
+            this.dataGridCars.Name = "dataGridCars";
+            this.dataGridCars.Size = new System.Drawing.Size(710, 284);
+            this.dataGridCars.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(38, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Current Inventory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridCars);
+            this.Controls.Add(this.btnExportToExcel);
+            this.Controls.Add(this.btnAddNewCar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCars)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddNewCar;
+        private System.Windows.Forms.Button btnExportToExcel;
+        private System.Windows.Forms.DataGridView dataGridCars;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
